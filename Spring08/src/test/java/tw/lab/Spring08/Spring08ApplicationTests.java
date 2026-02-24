0cc544f8b8cf5d2e270a5768d015fa3768ce2438
Kokoha-Mo <as86829@gmail.com>
2026-02-24 15:17:11 +0800

    feat:init Spring09

---
+++ Spring08/src/test/java/tw/lab/Spring08/Spring08ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.lab.Spring08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
