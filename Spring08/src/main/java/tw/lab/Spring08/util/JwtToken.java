9da3d7324448d1c19133813bb7b10bafa2ac5e0a
Kokoha-Mo <as86829@gmail.com>
2026-02-24 16:25:39 +0800

    feat:teacher/student寫字板

---
+++ Spring08/src/main/java/tw/lab/Spring08/util/JwtToken.java
@@ -0,0 +1,33 @@
+package tw.lab.Spring08.util;
+
+import java.security.Key;
+import java.util.Date;
+
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtToken {
+    private static final long EXP_TIME = 10 * 60 * 1000;
+    private static final String SECURT = "KokohaMo123456789zxcvbnmasdfghjklqwertyuiop";
+    private static final Key KEY = Keys.hmacShaKeyFor(SECURT.getBytes());
+
+    public static String createToken(String subject) {
+        String token = Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXP_TIME))
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
+    }
+
+    public static String parseToken(String token) {
+        JwtParser parser = Jwts.parserBuilder().setSigningKey(KEY).build();
+        String subject = parser.parseClaimsJws(token).getBody().getSubject();
+
+        return subject;
+    }
+}
---
+++ Spring08/src/main/resources/static/js/student.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+	let myDrawer = document.getElementById("myDrawer");
+
+	let webSocket = new WebSocket("ws://10.0.101.231:8080/ws");
+	let isConnect = false;
+
+	webSocket.onopen = function () {
+		isConnect = true;
+	}
+
+	webSocket.onmessage = function (event) {
+		if (isConnect) {
+			let mesgObj = JSON.parse(event.data)
+			if (mesgObj.isClear) {
+				clear();
+			} else {
+				if (mesgObj.isNewLine) {
+					newLine(mesgObj.x, mesgObj.y);
+				} else {
+					drawLine(mesgObj.x, mesgObj.y);
+				}
+			}
+		}
+	}
+
+	webSocket.onclose = function () {
+		console.log("onClose")
+		isConnect = false;
+	}
+	webSocket.onerror = function (event) {
+		console.log("onError" + event)
+	}
+	//--------------------------------------	
+	//畫圖邏輯:和paintcomponer類似
+	let ctx = myDrawer.getContext("2d");
+
+	function clear() {
+		ctx.clearRect(0, 0, myDrawer.width, myDrawer.height);
+	}
+
+	function newLine(x, y) {
+		ctx.beginPath();
+		ctx.lineWidth = 4;
+		ctx.moveTo(x, y);
+	}
+
+	function drawLine(x, y) {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
+
+}
